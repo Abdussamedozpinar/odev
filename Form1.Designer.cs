30e538e98db868de9fc85fea5243298d0b76d1d5
Abdüssamed Özpınar <wqewqe819@gmail.com>
2021-04-25 06:58:26 +0300

    github.com çözüyorum

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,13 +29,71 @@ namespace ödev
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.baslik = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.haber = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.baslik,
+            this.haber});
+            this.dataGridView1.Location = new System.Drawing.Point(134, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.Size = new System.Drawing.Size(1140, 425);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(546, 479);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(317, 102);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Haberler Gelsin";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // baslik
+            // 
+            this.baslik.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.baslik.Frozen = true;
+            this.baslik.HeaderText = "Haber Başlığı";
+            this.baslik.Name = "baslik";
+            this.baslik.ReadOnly = true;
+            // 
+            // haber
+            // 
+            this.haber.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.haber.Frozen = true;
+            this.haber.HeaderText = "Ana Haber";
+            this.haber.Name = "haber";
+            this.haber.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1383, 624);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn baslik;
+        private System.Windows.Forms.DataGridViewTextBoxColumn haber;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace ödev
 {
@@ -15,6 +17,50 @@ namespace ödev
         public Form1()
         {
             InitializeComponent();
+        }
+
+        string sitelink = "https://www.cnnturk.com/feed/rss/all/news";
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            XmlDocument doc1 = new XmlDocument();
+            doc1.Load(sitelink);
+            XmlElement root = doc1.DocumentElement;
+            XmlNodeList nodes = root.SelectNodes("channel/item");
+
+
+            foreach (XmlNode node in nodes)
+            {
+
+
+
+                string baslik = node["title"].InnerText;
+                string haber = node["description"].InnerText;
+
+
+
+
+                DataGridViewRow row = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+                row.Cells[0].Value = baslik;
+                row.Cells[1].Value = haber;
+                dataGridView1.Rows.Add(row);
+
+
+                string fileName = @"C:\deneme\deneme.txt";
+                FileStream fs = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.Write);
+                fs.Close();
+                File.AppendAllText(fileName, Environment.NewLine + "Başlığı::" + baslik + Environment.NewLine + "Haber içeriği::" + haber + Environment.NewLine);
+
+
+
+            }
+
+
+
+
+
         }
     }
 }
